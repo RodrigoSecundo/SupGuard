2b8412ff0ab104b52ba4e90db4dfc5c113265833
Rodrigo Secundo Aráujo <65463632+RodrigoSecundo@users.noreply.github.com>
2025-05-09 12:04:40 -0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    telefone = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
+    nome = db.Column(db.String(255), nullable=False)
+    telefone = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
  
