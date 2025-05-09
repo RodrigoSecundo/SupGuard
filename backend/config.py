a528c35e562a31e75388ffc982bfa3108988786d
Rodrigo Secundo Aráujo <65463632+RodrigoSecundo@users.noreply.github.com>
2025-05-09 12:01:03 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:admin123@localhost:5432/supguard")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://seu.usuario:sua.senha@localhost:5432/supguard")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
